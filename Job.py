a28402eef169576f690bb42501a1edc0ae312dcb
Kateryna <katerinashkrob62@gmail.com>
2023-11-19 17:01:06 +0100

    Make a new text file and link

--- Job.py
+++ Job.py
@@ -1,6 +1,7 @@
 class Job:
-    def __init__(self, title, city, time):
+    def __init__(self, title, city, time, link):
         self.title = title
         self.city = city
         self.time = time
+        self.link = link
 
--- ProjectPython.py
+++ ProjectPython.py
@@ -4,15 +4,19 @@ from Job import Job
 url = 'https://www.linkedin.com/jobs/search?keywords=Fahrer&location=Germany&geoId=101282230&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
-jobs = soup.find_all("div", {"class": "base-search-card__info"})
-titles = []
-for job in jobs:
-    title = job.find("h3", {"class": "base-search-card__title"})
-    titles.append(title.text.strip())
-    city = job.find("span", {"class": "job-search-card__location"})
-    time = job.time["datetime"]
-    print("Arbeit: " + title.text.strip())
-    print("Stadt: " + city.text.strip())
-    print("Zeit: " + time)
-
-    jobObject = Job('title', 'city', 'time')
+jobsHtml = soup.find_all("div", {"class": "base-card"})
+jobs = []
+for jobStructure in jobsHtml:
+    title = jobStructure.find("h3", {"class": "base-search-card__title"}).text.strip()
+    city = jobStructure.find("span", {"class": "job-search-card__location"}).text.strip()
+    time = jobStructure.time["datetime"]
+    link = jobStructure.a["href"]
+    jobObject = Job(title, city, time, link)
+    jobs.append(jobObject)
+    # print(jobObject.title)
+    # print(jobObject.city)
+    # print(jobObject.time)
+print(len(jobs))
+with open('results/readme.txt', 'w') as file:
+    for job in jobs:
+         file.write(job.title + ":" + job.link + "\n")
